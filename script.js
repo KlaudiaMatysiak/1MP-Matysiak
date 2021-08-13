a739ade645e900a771a4c119c15e38f99832f109
KlaudiaMatysiak <klatysiak@gmail.com>
2021-08-13 14:47:07 +0000

    Move javascript file into assets directory

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const menuLIST = document.getElementById('menu-list');
-
-hamburger.addEventListener('click', () => {
-    menuLIST.classList.toggle('show');
-});
